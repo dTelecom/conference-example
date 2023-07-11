27140c1da239dd623f437d308c9dd0577ef98e58
Roman <email>
2023-07-11 17:39:41 +0600

    node cache

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -2,6 +2,7 @@ import { WebhookReceiver } from "@dtelecom/server-sdk-js";
 import type { NextApiRequest, NextApiResponse } from "next";
 import jwt_decode from "jwt-decode";
 import { getNodeByAddress } from "@dtelecom/server-sdk-js/dist/contract/contract";
+import prisma from "@/lib/prisma";
 
 interface JwtKey {
   iss: string;
@@ -13,11 +14,39 @@ export default async function handler(
 ) {
   if (req.method === "POST" && req.headers.authorization) {
     const jwt = jwt_decode<JwtKey>(req.headers.authorization);
-    const node = await getNodeByAddress(jwt.iss);
+    let node = await prisma.node.findFirst({
+      where: {
+        iss: jwt.iss
+      }
+    });
+
+    const now = new Date().getTime();
+    if (!node || node.expiresAt.getTime() < now) {
+      const nodeByAddress = await getNodeByAddress(jwt.iss);
+
+      const expiresAt = new Date(now + 86400000);
+
+      node = await prisma.node.upsert({
+        where: {
+          iss: jwt.iss
+        },
+        update: {
+          key: nodeByAddress.key,
+          expiresAt
+        },
+        create: {
+          iss: jwt.iss,
+          key: nodeByAddress.key,
+          expiresAt
+        }
+      });
+    }
+
     const receiver = new WebhookReceiver(jwt.iss, node.key.replace("0x", ""));
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
     const event = receiver.receive(req.body, req.headers.authorization);
     console.log("event", event);
   }
+
   res.status(200).send("ok");
 }
