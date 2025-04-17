72e007b78b4577fd5dfc5a11d105b5824c316c37
Roman <email>
2025-04-17 13:09:45 +0300

    domain setting

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -8,9 +8,8 @@ export async function setCookie(name: string, value: string, domain: string) {
   const date = new Date();
   date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000));
   cookieStore.set(name, value, {
-    // expires: date,
-    // path: '/',
-    // domain: extractDomain(domain),
+    expires: date,
+    domain: process.env.NODE_ENV === 'development' ? undefined: extractDomain(domain),
   })
 }
 
