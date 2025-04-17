1ebae1ff451f21ac9ab5d39ab4d5d849277831b8
Roman <email>
2025-04-17 13:34:27 +0300

    cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -9,17 +9,6 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
-  /**
-   * If you have the "experimental: { appDir: true }" setting enabled, then you
-   * must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
   transpilePackages: [
     '@dtelecom/components-react',
     '@dtelecom/livekit-client',
